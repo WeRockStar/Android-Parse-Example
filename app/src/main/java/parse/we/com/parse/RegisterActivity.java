e0bcb52bcda062dbb6eb849fe9ac69a98ebc2718
comfreestyle <vr_freestyle@outlook.com>
2015-08-19 16:54:04 +0700

    Initialize Widget

--- app/src/main/java/parse/we/com/parse/RegisterActivity.java
+++ app/src/main/java/parse/we/com/parse/RegisterActivity.java
@@ -4,6 +4,7 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
 import android.widget.EditText;
 
 public class RegisterActivity extends Activity {
@@ -11,13 +12,17 @@ public class RegisterActivity extends Activity {
     protected EditText mUsername;
     protected EditText mEmail;
     protected EditText mUserpassword;
+    protected Button mRegisterButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
 
-        
+        mUsername = (EditText)findViewById(R.id.usernameRegister);
+        mEmail = (EditText)findViewById(R.id.emailRegister);
+        mUserpassword = (EditText)findViewById(R.id.passwordRegister);
+        mRegisterButton = (Button)findViewById(R.id.buttonSignUp);
     }
 
 }
