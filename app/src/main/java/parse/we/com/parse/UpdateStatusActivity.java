ee359d3024c214debba7aa140f2cb9c70f785892
comfreestyle <vr_freestyle@outlook.com>
2015-08-19 19:38:56 +0700

    Save status to parse

--- app/src/main/java/parse/we/com/parse/UpdateStatusActivity.java
+++ app/src/main/java/parse/we/com/parse/UpdateStatusActivity.java
@@ -1,11 +1,22 @@
 package parse.we.com.parse;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import com.parse.ParseException;
+import com.parse.ParseObject;
+import com.parse.ParseUser;
+import com.parse.SaveCallback;
 
 public class UpdateStatusActivity extends Activity {
 
@@ -16,10 +27,66 @@ public class UpdateStatusActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_update_status);
-        
+
         postButton = (Button) findViewById(R.id.postButton);
         updateStatusEditText = (EditText) findViewById(R.id.updateStatusEditText);
 
+        postButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //get username current
+                ParseUser cuParseUser = ParseUser.getCurrentUser();
+                String username = cuParseUser.getUsername().toString();
+                //status text
+                String status = updateStatusEditText.getText().toString().trim();
+                try {
+                    if (!status.isEmpty()) {
+                        //save to parse
+                        ParseObject statusObj = ParseObject.create("Status");
+                        statusObj.put("Status", status);
+                        statusObj.put("username", username);
+                        statusObj.saveInBackground(new SaveCallback() {
+                            @Override
+                            public void done(ParseException e) {
+                                if (e == null) {
+                                    Intent intent = new Intent(UpdateStatusActivity.this, HomepageActivity.class);
+                                    startActivity(intent);
+                                } else {
+                                    AlertDialog.Builder builder = new AlertDialog.Builder(UpdateStatusActivity.this);
+                                    builder.setTitle("Warning...");
+                                    builder.setMessage("Status not working...");
+                                    builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                                        @Override
+                                        public void onClick(DialogInterface dialog, int which) {
+                                            dialog.dismiss();
+                                            dialog.cancel();
+                                        }
+                                    });
+                                    AlertDialog alertDialog = builder.create();
+                                    alertDialog.show();
+                                }
+                            }
+                        });
+                    } else {
+                        AlertDialog.Builder builder = new AlertDialog.Builder(UpdateStatusActivity.this);
+                        builder.setTitle("Warning...");
+                        builder.setMessage("Please input your status");
+                        builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                dialog.dismiss();
+                                dialog.cancel();
+                            }
+                        });
+                        AlertDialog alertDialog = builder.create();
+                        alertDialog.show();
+                    }
+                } catch (Exception ex) {
+                    Log.e("ERROR", ex.getMessage().toString());
+                }
+
+            }
+        });
     }
 
     @Override
