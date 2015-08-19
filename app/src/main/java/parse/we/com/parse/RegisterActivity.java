9111f5ba8de2dd7e3bb7470c7e082145ff868e56
comfreestyle <vr_freestyle@outlook.com>
2015-08-19 16:51:29 +0700

    Register Design

--- app/src/main/java/parse/we/com/parse/RegisterActivity.java
+++ app/src/main/java/parse/we/com/parse/RegisterActivity.java
@@ -4,13 +4,20 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.EditText;
 
 public class RegisterActivity extends Activity {
+
+    protected EditText mUsername;
+    protected EditText mEmail;
+    protected EditText mUserpassword;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        
     }
 
 }
