2407a49e33abf4f4a72b59afad3bf317a4396a25
comfreestyle <vr_freestyle@outlook.com>
2015-08-19 21:02:19 +0700

    Implemet Calligraphy

--- app/src/main/java/parse/we/com/parse/ParseInitial.java
+++ app/src/main/java/parse/we/com/parse/ParseInitial.java
@@ -4,6 +4,8 @@ import android.app.Application;
 
 import com.parse.Parse;
 
+import uk.co.chrisjenx.calligraphy.CalligraphyConfig;
+
 /**
  * Created by Kotchaphan Muangsan on 19/8/2558.
  */
@@ -13,6 +15,10 @@ public class ParseInitial extends Application {
         super.onCreate();
         Parse.initialize(this, "95I4LvWem7H4dIYI1MfnWRQjRdcziQHh76Rh6x9A", "yKUWKoKqe1hnGtsX6io12mZOSjkGdN5r7RliyR6Z");
 
+        CalligraphyConfig.initDefault(new CalligraphyConfig.Builder()
+                .setDefaultFontPath("fonts/Roboto-Bold.ttf")
+                .setFontAttrId(R.attr.fontPath)
+                .build());
 
     }
 }
