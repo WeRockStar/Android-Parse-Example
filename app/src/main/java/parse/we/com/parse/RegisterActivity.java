598fdee50b566a09846e15cef4bae3a849c8ace0
comfreestyle <vr_freestyle@outlook.com>
2015-08-19 17:16:22 +0700

    Sign Up Success

--- app/src/main/java/parse/we/com/parse/RegisterActivity.java
+++ app/src/main/java/parse/we/com/parse/RegisterActivity.java
@@ -1,11 +1,19 @@
 package parse.we.com.parse;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import com.parse.Parse;
+import com.parse.ParseException;
+import com.parse.ParseUser;
+import com.parse.SignUpCallback;
 
 public class RegisterActivity extends Activity {
 
@@ -19,10 +27,40 @@ public class RegisterActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
 
-        mUsername = (EditText)findViewById(R.id.usernameRegister);
-        mEmail = (EditText)findViewById(R.id.emailRegister);
-        mUserPassword = (EditText)findViewById(R.id.passwordRegister);
-        mRegisterButton = (Button)findViewById(R.id.buttonSignUp);
+        Parse.initialize(this, "95I4LvWem7H4dIYI1MfnWRQjRdcziQHh76Rh6x9A", "yKUWKoKqe1hnGtsX6io12mZOSjkGdN5r7RliyR6Z");
+        mUsername = (EditText) findViewById(R.id.usernameRegister);
+        mEmail = (EditText) findViewById(R.id.emailRegister);
+        mUserPassword = (EditText) findViewById(R.id.passwordRegister);
+        mRegisterButton = (Button) findViewById(R.id.buttonSignUp);
+
+        //TODO Click
+        mRegisterButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //TODO Save
+                String txtUser = mUsername.getText().toString().trim();
+                String txtEmail = mEmail.getText().toString().trim();
+                String txtPassword = mUserPassword.getText().toString().trim();
+
+                ParseUser user = new ParseUser();
+                user.setUsername(txtUser);
+                user.setPassword(txtPassword);
+                user.setEmail(txtEmail);
+                user.signUpInBackground(new SignUpCallback() {
+                    @Override
+                    public void done(ParseException e) {
+                        if (e == null) {
+                            //signup success
+                            Intent intent = new Intent(RegisterActivity.this, HomepageActivity.class);
+                            startActivity(intent);
+                        } else {
+                            //signup error
+                            Toast.makeText(RegisterActivity.this, "Error", Toast.LENGTH_LONG).show();
+                        }
+                    }
+                });
+            }
+        });
     }
 
 }
